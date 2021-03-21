95e7d7757ad4087c2cc282b2289577ec1ba6848c
sultan <sultan.panar@gmail.com>
2021-03-22 05:41:55 +0600

    some changes in settings and admin

--- coreapp/admin.py
+++ coreapp/admin.py
@@ -1,26 +1,22 @@
 from django.contrib import admin
 from .models import *
-from django import forms
+from django.forms import ModelChoiceField
 
 
-class NoteBookCategoryChoiceField(forms.ModelChoiceField):
-    pass
 
 class NoteBookAdmin(admin.ModelAdmin):
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
-            return NoteBookCategoryChoiceField(Category.objects.filter(slug='notebooks'))
+            return ModelChoiceField(Category.objects.filter(slug='notebooks'))
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
-class SmartphoneCategoryChoiceField(forms.ModelChoiceField):
-    pass
 
 class SmartphoneAdmin(admin.ModelAdmin):
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == 'category':
-            return SmartphoneCategoryChoiceField(Category.objects.filter(slug='smartphones'))
+            return ModelChoiceField(Category.objects.filter(slug='smartphones'))
         return super().formfield_for_foreignkey(db_field, request, **kwargs)
 
 admin.site.register(Category)
